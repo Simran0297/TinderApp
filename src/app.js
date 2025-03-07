4849a2f51609e7fd31a28238c7b1640f5659ae42
Simran0297 <168592699+Simran0297@users.noreply.github.com>
2025-03-08 03:14:59 +0530

    CHanges with API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,140 @@
 const express = require("express");
-
 const app = express();
 
-const {connectDB} = require("./config/database");
+const User = require("./models/user");
 
+const {connectDB} = require("./config/database");
+//for changing json string to JS object for all the API's
+app.use(express.json());
+//USer using FirstName
+app.get("/user", async (req,res)=>{
+    
+    const userfirstName = req.body.firstName;
+    try{
+        const user = await User.find({firstName: userfirstName});
+        if(user.length ===0)
+
+            {
+                res.send("User not Found")
+            }
+            else{
+                res.send(user);
+            }
+    }
+    catch(err)
+    {
+        res.status(404).send("SOmething went Wrong")
+    }
+})
+//Feed API
+app.get("/feed", async (req,res)=>{
+    try{
+        const allUsers= await User.find({})
+        if(allUsers.length ===0)
+        {
+            res.status(404).send("No users Found")
+        }
+        else{
+            res.send(allUsers)
+        }
+    }
+    catch(err)
+    {
+        res.send("Something Went Wrong")
+    }
+  
+})
+
+//Find USer by ID
+app.get("/userId", async(req,res)=>{
+    const inputId = req.body.userId;
+    try{
+    const user = await User.findById(inputId);
+
+    if(user.length===0)
+    {
+        res.status(404).send("User Not Found")
+    }
+    else{
+        res.send(user);
+    }
+}
+catch(err)
+{
+    res.send("Something Went Wrong")
+}
+
+})
+
+function validateFirstName(name){
+        const regex = /^([a-zA-Z]+)$/;
+        const isValid = regex.test(name);
+        return isValid;
+        
+}
+app.post("/signup", async(req,res)=>{
+    //Creating a new instance of the user Model
+    const user= new User(req.body);
+try{
+    //firstName Validation
+    if(!validateFirstName(req.body.firstName))
+    {
+        throw new Error("Invalid Name Format")
+    }
+    await user.save();
+    res.send("User added successfully")
+}
+catch(err)
+{
+    res.send(err.message)
+  }});
+
+//Delete user
+app.delete("/user", async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+    const deletedUser = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully")
+    }
+    catch(err)
+    {
+        res.send("Cant delete user")
+    }
+})
+
+//FindAndupdateUser
+
+app.patch("/user/:userId", async(req,res)=>{
+    const userId= req.params?.userId;
+    
+    const updateData = req.body;
+  
+    console.log(userId,updateData)
+    try{
+    // const ALLOWED_UPDATES = ["about","gender","age","skills"];
+   
+    // const isUpdateAllowed = Object.keys(updateData).every((k)=>{
+    //     console.log(k);
+    //     ALLOWED_UPDATES.includes(k);
+        
+    // })
+    
+    // if(!isUpdateAllowed)
+    // {
+    //     throw new Error("Update now allowed "+ err.message);
+    // }
+    if(data?.skills?.length>10)
+    {
+        throw new Error("Skills Cant be more than 10")
+    }
+    const updateUSer = await User.findByIdAndUpdate(userId,updateData,{runValidators:true});
+    res.send("Data Updated Successfully")
+    }
+    catch(err)
+    {
+        res.send("Update Failed " + err.message)
+    }
+})
 
 connectDB().then(()=>{
     console.log("Data Connection esablished")
@@ -15,5 +146,4 @@ connectDB().then(()=>{
     console.error("Some error")
 })
 
-// mongodb+srv://simranarora839:Simran%40297@simrandata.3l6si.mongodb.net/
-// const URI = "mongodb+srv://simranarora839:Simran@297@simrandata.3l6si.mongodb.net/"
+
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,62 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required: true,
+        minLength: 3,
+        maxLength: 50
     },
     lastName:{
-        type : String
+        type : String 
     },
-    emailId : {
-        tye: String
-    },
+    
     password : {
-        type: String
+        type: String,
+        required:true
     },
     age:{
-        type: Number
+        type: Number,
+        min : 18
+    },
+    emailId:{
+        type: String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        validate(value)
+        {
+            if(!validator.isEmail(value))
+            {
+                throw new Error ("Invalid Email Address")
+            }
+        }
+    
     },
     gender: {
-        type: String
+        type: String,
+        lowercase:true,
+        validate(value)
+        {
+            if(!["male","female","others"].includes(value))
+            {
+                throw new Error("Gender is not valid Value")
+            }
+        }
+    },
+    photourl :{
+        type: String,
+        default : "https://pinnacle.works/dummy-image/"
+    },
+    about:{
+        type: String,
+        default : "This is the default about me of the user!!"
+    },
+    skills:{
+        type: [String]
     }
-})
+},{timestamps:true})
 
 const user = mongoose.model("User",userSchema);
 
